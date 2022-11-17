a5feaf82139b74e41c19fbbe0f8629cdefb73572
eqber00 <eqber00@gmail.com>
2022-11-17 13:02:16 +0300

    header function added.

--- components/common/appHeader.vue
+++ components/common/appHeader.vue
@@ -1,20 +1,36 @@
 <template>
-  <header class="header">
-    <div class="container">
-      <div class="header__wrapper">
-        <div class="header__logo">
-          <img src="~/static/images/qbr-logo.svg" alt="" />
-        </div>
-        <div class="header__menu">
-          <div class="header__menu-line"></div>
-          <div class="header__menu-line"></div>
-          <div class="header__menu-line"></div>
+  <headroom
+    :speed="400"
+    :upTolerance="10"
+    :downTolerance="10"
+    :classes="{
+      pinned: 'header--pinned',
+      unpinned: 'header--unpinned',
+      top: 'header--top',
+      notTop: 'header--not-top',
+    }"
+  >
+    <header class="header">
+      <div class="container">
+        <div class="header__wrapper">
+          <div class="header__logo">
+            <img src="~/static/images/qbr-logo.svg" alt="" />
+          </div>
+          <div class="header__menu">
+            <div class="header__menu-line"></div>
+            <div class="header__menu-line"></div>
+            <div class="header__menu-line"></div>
+          </div>
         </div>
       </div>
-    </div>
-  </header>
+    </header>
+  </headroom>
 </template>
 
 <script>
-export default {};
+import { headroom } from "vue-headroom";
+
+export default {
+  components: { headroom },
+};
 </script>
