15b4640b0d863539f12f35d2c22de93c04f1203c
eqber00 <eqber00@gmail.com>
2022-11-18 20:40:14 +0300

    particle.js background added, hero__text animation added.

--- components/index/hero.vue
+++ components/index/hero.vue
@@ -4,8 +4,20 @@
       <div class="hero__wrapper">
         <div class="hero__text">
           <h1 class="txt txt--rem128 txt--font900 c-white">
-            Good design is about <span class="c-primary">process</span>, not
-            <span class="c-secondary">product</span>.
+            <div class="text-line">
+              <p class="text-line-inner">Good design is</p>
+            </div>
+            <div class="text-line">
+              <p class="text-line-inner">
+                about <span class="c-primary">process</span>,
+              </p>
+            </div>
+            <div class="text-line">
+              <p class="text-line-inner">
+                not
+                <span class="c-secondary">product</span>.
+              </p>
+            </div>
           </h1>
         </div>
         <div class="hero__scroll">
@@ -19,9 +31,11 @@
 <script>
 export default {
   mounted() {
-    const heroText = document.querySelector(".hero__text .txt");
+    const heroText = document.querySelectorAll(".hero__text .text-line-inner");
     setTimeout(() => {
-      heroText.classList.add("loaded");
+      heroText.forEach((e) => {
+        e.classList.add("loaded");
+      });
     }, 1500);
   },
 };
--- layouts/default.vue
+++ layouts/default.vue
@@ -7,16 +7,96 @@
       <div class="cursor__txt"></div>
       <div class="cursor__icon"></div>
     </div>
+    <Particles
+      id="tsparticles"
+      :particlesInit="particlesInit"
+      :particlesLoaded="particlesLoaded"
+      :options="particleOptions"
+    />
   </div>
 </template>
 
 <script>
+import Vue from "vue";
 import appHeader from "../components/common/appHeader.vue";
 import appFooter from "../components/common/appFooter.vue";
+import Particles from "particles.vue";
+import { loadFull } from "tsparticles";
+Vue.use(Particles);
 export default {
   components: {
     appHeader,
     appFooter,
+  },
+  data() {
+    return {
+      particleOptions: {
+        particles: {
+          number: {
+            value: 500,
+            density: {
+              enable: true,
+              value_area: 1000,
+            },
+          },
+          color: {
+            value: "#ffffff",
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 0,
+              color: "#ffffff",
+            },
+            polygon: {
+              nb_sides: 8,
+            },
+          },
+          opacity: {
+            value: 0.7,
+            random: false,
+            anim: {
+              enable: true,
+              speed: 0.2,
+              opacity_min: 0.1,
+              sync: false,
+            },
+          },
+          size: {
+            value: 3,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 3,
+              size_min: 0,
+              sync: false,
+            },
+          },
+          line_linked: {
+            enable: false,
+            distance: 100,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 1,
+          },
+          move: {
+            enable: true,
+            speed: 0.1,
+            direction: "none",
+            random: true,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 1200,
+            },
+          },
+        },
+        detectRetina: true,
+      },
+    };
   },
   mounted() {
     const dragMoveField = document.querySelectorAll(".js-drag-move-field");
@@ -58,6 +138,14 @@ export default {
         ease: "slow",
       });
     }
+  },
+  methods: {
+    particlesInit(engine) {
+      loadFull(engine);
+    },
+    particlesLoaded(container) {
+      console.log("Particles container loaded", container);
+    },
   },
 };
 </script>
