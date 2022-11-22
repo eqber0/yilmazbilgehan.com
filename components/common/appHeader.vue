281b6b6865f2878f43d863afd32589b825907887
eqber00 <eqber00@gmail.com>
2022-11-22 10:19:18 +0300

    added header menu.

--- components/common/appHeader.vue
+++ components/common/appHeader.vue
@@ -10,16 +10,26 @@
       notTop: 'header--not-top',
     }"
   >
-    <header class="header">
+    <header ref="header" class="header">
       <div class="container">
         <div class="header__wrapper">
           <div class="header__logo">
             <img src="~/static/images/qbr-logo.svg" alt="" />
           </div>
-          <div class="header__menu">
-            <div class="header__menu-line"></div>
-            <div class="header__menu-line"></div>
-            <div class="header__menu-line"></div>
+          <div @click="openMenu" class="header__menu-btn">
+            <div class="header__menu-btn-line"></div>
+            <div class="header__menu-btn-line"></div>
+          </div>
+          <div ref="headerMenu" class="header__menu">
+            <div class="header__menu__item c-white txt txt--rem24 txt--font400">
+              <NuxtLink to="/">Home</NuxtLink>
+            </div>
+            <div class="header__menu__item c-white txt txt--rem24 txt--font400">
+              <NuxtLink to="/works">Works</NuxtLink>
+            </div>
+            <div class="header__menu__item c-white txt txt--rem24 txt--font400">
+              <NuxtLink to="/contact">Contact</NuxtLink>
+            </div>
           </div>
         </div>
       </div>
@@ -32,5 +42,13 @@ import { headroom } from "vue-headroom";
 
 export default {
   components: { headroom },
+  methods: {
+    openMenu(e) {
+      const header = this.$refs.header;
+      const headerMenu = this.$refs.headerMenu;
+      headerMenu.classList.toggle("opened");
+      header.classList.toggle("menu-opened");
+    },
+  },
 };
 </script>
