5bf5f6ae7ea923b0203af1bfacdd4a936d69680e
eqber00 <eqber00@gmail.com>
2023-02-12 19:24:51 +0300

    fancybox animation fixed.

--- plugins/fancyapps-ui.js
+++ plugins/fancyapps-ui.js
@@ -1,3 +1,11 @@
 if (process.client) {
   window.fancybox = require("@fancyapps/ui");
+  window.fancybox.Fancybox.bind("[data-fancybox]", {
+    Image: {
+      ignoreCoveredThumbnail: true,
+    },
+    infinite: false,
+    Escape: "close",
+    Delete: "close",
+  });
 }
