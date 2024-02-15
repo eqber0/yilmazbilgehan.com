198bf050bb90bf66e50d7d20c4bf6c45affae88b
Bilgehan Yılmaz <eqber00@gmail.com>
2024-02-15 11:11:26 +0300

    nuxt.config.ts update, SEO meta tags added.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,31 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      title: "Bilgehan Yılmaz Portfolio",
+      title: "Bilgehan Yılmaz",
+      meta: [
+        {
+          name: "description",
+          content:
+            "A Frontend Developer based in Turkey, passionate about crafting engaging and visually appealing websites and web applications. Continually striving for excellence, leveraging the latest technologies to deliver compelling and seamless user experiences.",
+        },
+
+        { name: "og:title", content: "Bilgehan Yılmaz" },
+        { name: "og:type", content: "website" },
+        { name: "og:url", content: "https://yilmazbilgehan.com" },
+        {
+          name: "og:image",
+          content:
+            "https://firebasestorage.googleapis.com/v0/b/yilmazbilgehan-7c443.appspot.com/o/og-image.png?alt=media&token=b4b5b3c8-deb4-4a0a-b57f-bec23186bc0e",
+        },
+        {
+          name: "og:desciption",
+          content:
+            "A Frontend Developer based in Turkey, passionate about crafting engaging and visually appealing websites and web applications. Continually striving for excellence, leveraging the latest technologies to deliver compelling and seamless user experiences.",
+        },
+        { name: "og:image:width", content: "1230" },
+        { name: "og:image:height", content: "730" },
+      ],
+
       script: [
         {
           src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js",
